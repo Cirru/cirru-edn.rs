9a9c2743edea4c9f48c36c94a1a2c7e0c7baa9e9
tiye <jiyinyiyong@gmail.com>
2024-05-19 18:17:01 +0800

    expose EdnTag structure; tag 0.6.8

--- src/tag.rs
+++ src/tag.rs
@@ -17,7 +17,7 @@ use std::{
 #[derive(fmt::Debug, Clone)]
 pub struct EdnTag(
   /// which means there will be a limit of the count of all tags
-  Arc<str>,
+  pub Arc<str>,
 );
 
 impl fmt::Display for EdnTag {
